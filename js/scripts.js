a3ffb64ef3e7b235e2a7e4d107788c3a679025b4
AveryIB <144458003+AveryIB@users.noreply.github.com>
2023-10-24 15:19:46 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,32 @@
 // Use this file to add JavaScript to your project
 
 <html> 
-	<!-- Carousel -->
-		<div id="myCarousel" class="carousel slide" data-ride="carousel">
-
-	<!-- Carousel Indicators -->
-		<li data-target="#myCarousel" data-slide-to="1"></li>
-
-	<!-- Carousel Controls -->
-		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
 </html>
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}

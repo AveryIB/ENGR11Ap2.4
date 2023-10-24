9f4400638c98420e0cc74ea8497a1433b9209608
AveryIB <144458003+AveryIB@users.noreply.github.com>
2023-10-24 15:03:36 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,12 @@
 // Use this file to add JavaScript to your project
 
 <html> 
-	<heading>
-		test
-	</heading>
+	<!-- Carousel -->
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+	<!-- Carousel Indicators -->
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+
+	<!-- Carousel Controls -->
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
 </html>
